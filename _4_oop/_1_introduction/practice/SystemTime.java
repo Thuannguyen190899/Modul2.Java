d57b3003f26cfc183a5c2fb8f08008ff291cea35
Thuannguyen190899 <93263105+Thuannguyen190899@users.noreply.github.com>
2022-01-10 20:52:10 +0700

    up

--- _4_oop/_1_introduction/practice/SystemTime.java
+++
@@ -1,8 +0,0 @@
-package _1_introduction_java.practice;
-import java.util.Date;
-public class SystemTime {
-    public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println("Today is: " + now);
-    }
-}
